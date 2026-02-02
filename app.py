2ea7298c4ff67faa07238da01667bd1a69ee2cff
Bigillow <tsunheil@nycstudents.net>
2026-02-02 09:26:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,48 @@
-number = input("Is this number even or odd?")
-number = number%2
-if number == 1:
-    print("Odd")
-else:
-    print("Even")
+# number = input("Is this number even or odd?")
+# number = int(number)%2
+# if number == 1:
+#     print("Odd")
+# else:
+#     print("Even")
+
+
+
+
+
+# bill = input("How much is the bill?")
+# service = input("Is the service bad, okay, good , or great?")
+# service = service.lower()
+# if service == "bad":
+#     bill = int(bill)+(int(bill)*0)
+#     bill = "$" + str(bill)
+#     print(bill)
+# elif service == "okay":
+#     bill = int(bill)+(int(bill)*0.15)
+#     bill = "$" + str(bill)
+#     print(bill)
+# elif service == "good":
+#     bill = int(bill)+(int(bill)*0.2)
+#     bill = "$" + str(bill)
+#     print(bill)
+# elif service == "great":
+#     bill = int(bill)+(int(bill)*0.25)
+#     bill = "$" + str(bill)
+#     print(bill)
+
+number = input("Number to factor:")
+for i in range(21):
+    factor = []
+    dividing_number = 1
+    
+    number = int(number)%dividing_number
+    if number == 0: 
+        factor.apend(dividing_number)
+        dividing_number += 1
+    elif number == 1:
+        continue
+    print(factor)
+
+    
+
+
+
